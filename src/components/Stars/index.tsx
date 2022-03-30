31330b9369fe45e23b1890e78d3cd7817a422f48
Donald W. Boulton <donaldboulton@gmail.com>
2022-03-30 12:42:05 -0500

    ahooksgatsby 4.11.0

---
+++ src/components/Stars/index.tsx
@@ -0,0 +1,19 @@
+import Particles from "react-tsparticles"
+import type { Engine } from "tsparticles"
+import { loadStarsPreset } from "tsparticles-preset-stars"
+
+export class ParticlesContainer extends React.PureComponent<IProps> {
+  // this customizes the component tsParticles installation
+  async customInit(engine: Engine): Promise<void> {
+    // this adds the preset to tsParticles, you can safely use the
+    await loadStarsPreset(engine);
+  }
+
+  render() {
+    const options = {
+      preset: "stars",
+    };
+
+    return <Particles options={options} init={this.customInit} />;
+  }
+}
