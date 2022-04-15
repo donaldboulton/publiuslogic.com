a564f767ac9eced8ff3c8affc26695285bccaaaa
Donald W. Boulton <donaldboulton@gmail.com>
2022-04-14 19:17:03 -0500

    back to react 17.0.2

---
+++ src/components/Map/DivIconMarker.tsx
@@ -0,0 +1,51 @@
+import { Marker, MarkerProps } from "react-leaflet";
+import { DivIcon } from "leaflet";
+
+import L from "leaflet";
+import { createPortal } from "react-dom";
+import React, { useEffect } from "react";
+
+type ReactProps = { key: string | number } & { children: JSX.Element };
+
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/6e724310e0ed089cf4b8b261b8badf71206ad9e8/types/leaflet/index.d.ts#L75
+type ContainerProps = {
+  tagName: string;
+  className?: string;
+  container?: HTMLElement;
+};
+
+type DivIconMarkerProps = ReactProps & { marker: MarkerProps } & {
+  container: ContainerProps;
+};
+const DivIconMarker = ({
+  key,
+  children,
+  marker,
+  container,
+}: DivIconMarkerProps) => {
+  const { tagName, className } = container;
+  const element = L.DomUtil.create(tagName, className);
+  const divIcon = new DivIcon({ html: element });
+  const portal = createPortal(children, element);
+
+  useEffect(() => {
+    return () => {
+      L.DomUtil.remove(element);
+    };
+  });
+
+  const { position, eventHandlers } = marker;
+  return (
+    <>
+      {portal}
+      <Marker
+        key={key}
+        position={position}
+        icon={divIcon}
+        eventHandlers={eventHandlers}
+      >
+      </Marker>
+    </>
+  );
+};
+export default DivIconMarker;
