ea1c41c4708feaf27da6106422ffd516afde7294
Donald W. Boulton <donaldboulton@gmail.com>
2022-04-22 10:20:22 -0500

    screwed up

--- src/components/PostTitle/index.tsx
+++ src/components/PostTitle/index.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react'
 import * as CSS from 'csstype'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { letterContainerVariants, letterVariants } from './AnimationVariants'
-
-interface PostTitleProps {
-  children: string
-}
 
 const StyledTitleElement: CSS.Properties = {
   fontSize: 'calc(32px + (80 - 32) * ((100vw - 320px) / (1600 - 320)))',
@@ -19,43 +15,52 @@ const StyledTitleElement: CSS.Properties = {
   color: '#c026d3',
 }
 
-export const PostTitle: React.FC<PostTitleProps> = props => {
-  const { children } = props
+const AnimatedTitle = (props: {
+  children: string;
+  currentInView: boolean;
+}) => {
+  const { children, currentInView } = props
+
   return (
-    <motion.div>
+    <AnimatePresence>
+      {currentInView && (
         <StyledTitleElement
           {...props}
           variants={letterContainerVariants}
-          initial={'before'}
-          animate={'after'}
-          exit={'before'}
+          initial={"before"}
+          animate={"after"}
+          exit={"before"}
           key={children}
           aria-label={children}
+          aria-live={"polite"} // dont do this on production if it loops.
         >
-          {children.split(' ').map((word: string, wordI: number) => (
+          {children.split(" ").map((word: string, wordI: number) => (
             <div
               key={`word-${word}-${wordI}`}
               style={{
-                display: 'inline-block',
+                display: "inline-block"
               }}
             >
               {Array.from(word).map((letter, index) => (
                 <motion.span
                   key={`${index}-${letter}`}
                   style={{
-                    position: 'relative',
-                    display: 'inline-block',
-                    width: 'auto',
+                    position: "relative",
+                    display: "inline-block",
+                    width: "auto"
                   }} // Position elements
                   variants={letterVariants}
                 >
-                  {letter === ' ' ? '\u00A0' : letter}
+                  {letter === " " ? "\u00A0" : letter}
                 </motion.span>
               ))}
-              {'\u00A0'}
+              {"\u00A0"}
             </div>
           ))}
         </StyledTitleElement>
-    </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
+
+export default AnimatedTitle
