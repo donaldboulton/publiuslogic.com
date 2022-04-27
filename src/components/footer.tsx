7448d61a4213bb2fa7e3308a408f954b9513aa63
Donald W. Boulton <donaldboulton@gmail.com>
2022-04-27 04:19:05 -0500

    footer copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -188,7 +188,7 @@ export default function IndexFooter() {
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-gray-200 font-semibold py-1">
-              Copyright © {new Date().getFullYear()} Publiuslogic by{' '}
+              © {new Date().getFullYear()} Publiuslogic by{' '}
               <a
                 href="https://donboulton.com"
                 rel="noopener noreferrer"
