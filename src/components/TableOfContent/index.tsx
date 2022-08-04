dc9f51ad5cc6076719e2d1d21065a440b819e267
Donald W. Boulton <donaldboulton@gmail.com>
2022-08-04 14:42:08 -0500

    toc fix

--- src/components/TableOfContent/index.tsx
+++ src/components/TableOfContent/index.tsx
@@ -74,15 +74,25 @@ function useActiveId(headingIds) {
   return activeId
 }
 
+const virtualReference = {
+  getBoundingClientRect() {
+    return {
+      top: 10,
+      left: 10,
+      bottom: 20,
+      height: 60,
+    };
+  },
+};
 const TableOfContent = ({ headings }: TableOfContentProps) => {
   const idList = getIds(headings)
   const activeId = useActiveId(idList)
   // https://headlessui.dev/react/popover#positioning-the-panel
   const [referenceElement, setReferenceElement] = useState()
   const [popperElement, setPopperElement] = useState()
-  const { styles, attributes } = usePopper(referenceElement, popperElement)
+  const { styles, attributes } = usePopper(virtualReference, referenceElement, popperElement)
   return (
-    <div className="fixed left-1 mb-4 pb-4 md:left-1 z-10 top-2/4 w-32 h-2/4">
+    <div className="fixed left-1 mb-4 pb-4 md:left-1 z-10 top-3/4 w-32">
       <Popover as="div">
         {({ open }) => (
           <>
@@ -105,13 +115,13 @@ const TableOfContent = ({ headings }: TableOfContentProps) => {
               leaveFrom="transform scale-100 opacity-100"
               leaveTo="transform scale-95 opacity-0"
             >
-              <Popover.Panel className="w-52" ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+              <Popover.Panel className="relative w-64 bottom-1/4" ref={setPopperElement} style={styles.popper} {...attributes.popper}>
                 <div className="rounded-lg shadow-lg ring-1 mt-2 ml-2 mr-2 bg-gray-700  ring-black ring-opacity-5">
-                  <div className="text-gray-200 text-lg text-center underline underline-offset-2 decoration-wavy decoration-fuchsia-600">
+                  <div className="text-gray-200 text-xl text-center underline underline-offset-2 decoration-wavy decoration-fuchsia-600">
                     Table Of Contents
                   </div>
                   <WavyHr className="mt-1 mb-1" />
-                  <nav className="overflow-y-auto overflow-x-hidden nav-scroll h-96 w-auto">
+                  <nav className="overflow-y-auto overflow-x-hidden nav-scroll h-auto w-auto">
                     <ul className="flex flex-col">
                       {headings.map(heading => {
                         if (heading.depth > 4) {
@@ -119,7 +129,7 @@ const TableOfContent = ({ headings }: TableOfContentProps) => {
                         }
 
                         return (
-                          <li className="p-1 ml-1 mb-4 mt-2 mr-1 list-none" key={heading.value}>
+                          <li className="p-1 ml-1 mb-2 mt-2 mr-1 list-none" key={heading.value}>
                             <Link
                               className="hover:text-gray-300 transition duration-300 underline underline-offset-2 decoration-wavy decoration-fuchsia-600"
                               rel="noopener noreferrer"
