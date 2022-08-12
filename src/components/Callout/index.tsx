246f1b5143cccb29b80a01df6e58044dff4b94c4
Donald W. Boulton <donaldboulton@gmail.com>
2022-08-12 12:13:27 -0500

    fixed page load transitions of header callout

--- src/components/Callout/index.tsx
+++ src/components/Callout/index.tsx
@@ -2,6 +2,10 @@ import * as React from 'react'
 import * as CSS from 'csstype'
 import { CalloutProps } from './types'
 import { getVariantIcon } from '../../utils/utils'
+import { useInView } from 'react-intersection-observer'
+import { LazyMotion, m } from 'framer-motion'
+
+const loadFeatures = () => import('@/components/FramerFeatures').then(res => res.default)
 
 export const callOutWrapper: CSS.Properties = {
   ['--icon-color' as info]: '#9333ea',
@@ -69,37 +73,75 @@ const callout: CSS.Properties = {
   background: '#282A36',
   boxShadow: '6px 5px 5px rgb(147, 51, 234, 0.75)',
   opacity: '0.6',
-
-  variants: {
-    variant: {
-      info: {
-        background: '#9333ea',
-      },
-      danger: {
-        background: '#fa383826',
-      },
-    },
-  },
 }
 
 const Callout: React.FC<CalloutProps> = props => {
   const { children, variant, label, ...rest } = props
   const icon = label ? null : getVariantIcon(variant)
+  
+  const item = {
+    initial: { y: 20, opacity: 0 },
+    enter: {
+      y: 0,
+      opacity: 1,
+    },
+  }
+
+  const useAnimateOnInView = () => {
+    const controls = useAnimation()
+    const { ref, inView } = useInView()
+
+    useEffect(() => {
+      if (inView) {
+        controls.start('visible')
+      }
+      if (!inView) {
+        controls.start('hidden')
+      }
+    }, [controls, inView])
+
+    return { ref }
+  }
+  const [ref6, isVisible6] = useInView({
+    triggerOnce: true,
+    rootMargin: '-200px 0px',
+  })
+  const variants6 = {
+    visible: {
+      opacity: 1,
+      x: 0,
+    },
+    hidden: {
+      opacity: 0,
+      x: -200,
+    },
+  }
 
   return (
-    <div style={callout} variant={variant} {...rest}>
-      {icon ? (
-        <aside style={callOutWrapper} variant={variant}>
-          {icon}
-        </aside>
-      ) : null}
-      {label ? (
-        <aside style={calloutLabelWrapper} variant={variant}>
-          {label}
-        </aside>
-      ) : null}
-      {children}
-    </div>
+    <LazyMotion features={loadFeatures}>
+      <m.div
+        ref={ref6}
+        animate={isVisible6 ? 'visible' : 'hidden'}
+        variants={variants6}
+        transition={{ duration: 0.8, ease: 'easeOut' }}
+        key={6}
+        style={callout}
+        variant={variant}
+        {...rest}
+      >
+        {icon ? (
+          <aside style={callOutWrapper} variant={variant}>
+            {icon}
+          </aside>
+        ) : null}
+        {label ? (
+          <aside style={calloutLabelWrapper} variant={variant}>
+            {label}
+          </aside>
+        ) : null}
+        {children}
+      </m.div>
+    </LazyMotion>
   )
 }
 
