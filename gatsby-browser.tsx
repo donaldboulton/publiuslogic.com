7c6f4747d1b1ab298d521cd416326348d6153b3a
Donald W. Boulton <donaldboulton@gmail.com>
2022-08-27 22:11:25 -0500

    all logrocket

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -3,6 +3,9 @@ import type { GatsbyBrowser } from 'gatsby'
 import './src/styles/global.css'
 import Layout from './src/components/Layout'
 import { wrapRootElement as wrap } from './wrap-root-element'
+import LogRocket from 'logrocket'
+
+LogRocket.init('mcjbg9/publiuslogic')
 
 export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
